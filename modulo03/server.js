f6194e6ac4a032f04fa1563b04a24fbb7ffa88ab
João Inácio Neto <birobirobiro.dev@gmail.com>
2020-09-01 01:09:08 -0300

    Finalizando módulo 3.3

--- modulo03/server.js
+++ modulo03/server.js
@@ -2,21 +2,34 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 
 const server = express();
+const videos = require("./data")
 
 server.use(express.static('public'));
 
 server.set("view engine", "njk");
 
 nunjucks.configure("views", {
-  express:server
+  express:server,
+  autoescape: false
 })
 
 server.get("/", function(req, res) {
-  return res.render("about")
+  const about = {
+    avatar_url: "https://avatars2.githubusercontent.com/u/22185823?s=400&u=3187113eec6ce4161d62d1860d505a23c4752c05&v=4",
+    name: "João Inácio",
+    role: "Community Experience - Rocketseat",
+    description: 'Full Stack Developer, focado em trazer o melhor ensino para iniciantes em programação. Colaborador da <a href="https://rocketseat.com.br/" target="_blank">Rocketseat</a>',
+    links: [
+        { name: "Github", url: "https://github.com/birobirobiro" },
+        { name: "Twitter", url: "https://twitter.com/birobirobiro_/" },
+        { name: "Linkedin", url: "https://linkedin.com//in/joaoinacioneto/" }
+    ],
+  }
+  return res.render("about", { about })
 })
 
 server.get("/portfolio", function(req, res) {
-  return res.render("portfolio")
+  return res.render("portfolio", {items: videos})
 })
 
 server.listen(5000, function(){
