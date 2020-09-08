b46147d82327aaeb8bcc9d1f052d7f5df39428c5
João Inácio Neto <birobirobiro.dev@gmail.com>
2020-09-08 00:09:09 -0300

    Desafio 3.2 Finalizado :rocket:

---
+++ desafios-launchbase/modulo03/3-2/data.js
@@ -0,0 +1,24 @@
+module.exports = [
+{
+  id: "starter",
+  logo: "img/starter.svg",
+  price: "FREE",
+  modules: "80 módulos",
+  description: "Cursos 100% online e gratuitos para você entrar com o pé direito nas tecnologias mais desejadas do mercado."
+},
+{
+  id: "lauchbase",
+  logo: "img/launchbase.svg",
+  price: "R$ 800,00",
+  modules: "60 módulos",
+  description: "LaunchBase é o bootcamp da Rocketseat voltado para quem está iniciando na programação. Você irá aprender HTML, CSS, JavaScript, Banco de dados Relacional e introdução ao NodeJS."
+},
+{
+  id: "gostack",
+  logo: "img/gostack.svg",
+  price: "R$ 1.000,00",
+  modules: "300 módulos",
+  description: "GoStack é o bootcamp da Rocketseat voltado para quem já manja de programação. Nele você irá aprender a stack ReactJS, React Native, TypeScript e NodeJS."
+} 
+
+]
