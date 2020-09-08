0e102ed390f255dd2f5108d4bf3e882179c18275
João Inácio Neto <birobirobiro.dev@gmail.com>
2020-09-08 00:51:42 -0300

    Fazendo o desafio  3.3

---
+++ desafios-launchbase/modulo03/3-3/server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+
+const server = express();
+const courses = require("./data")
+
+server.use(express.static('public'));
+
+server.set("view engine", "njk");
+
+nunjucks.configure("views", {
+  express:server,
+  autoescape: false,
+  noCache: true
+})
+
+server.get("/", function(req, res) {
+  return res.render("courses", {courses});
+})
+
+server.get("/courses2/:id", function(req, res) {
+  const id = req.params.id;
+
+  return res.render("courses2", {id});
+});
+
+server.get("/about", function(req, res) {
+  const about = { 
+        logo: "img/logo.jpg", 
+        links: [
+        {
+          name: "Github",   
+          url: "https://github.com/rocketseat/"
+        },
+        {
+          name: "Instagram", 
+          url: "https://instagram.com/rocketseat_oficial/"
+        },
+
+        {
+          name: "Facebok",
+          url: "https://facebook.com/rocketseat/"
+        }
+      ],
+      techs: [
+        "ReactJS", 
+        "React Native", 
+        "NodeJS", 
+        "TypeScript"
+      ],
+      description: `Transforme sua carreira e seja um programador desejado no mercado, dominando as ferramentas mais modernas de desenvolvimento web e mobile.`
+    }
+
+  return res.render("about", {about});
+})
+
+server.use(function(req, res) {
+  res.status(404).render("not-found");
+});
+
+server.listen(5000, function(){
+  console.log("Server is running...🚀") 
+})
