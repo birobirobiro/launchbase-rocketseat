151cb7525a6fee9261f44e77e8066d94385ec97f
João Inácio Neto <birobirobiro.dev@gmail.com>
2020-11-04 01:28:30 -0300

    :tada: Desafio refatorando Foodfy finalizado :rocket:

--- desafios-launchbase/modulo03/3-0/public/script.js
+++ desafios-launchbase/modulo03/3-0/public/script.js
@@ -5,3 +5,28 @@ for(let i = 0; i < receitas.length; i++) {
         window.location.href = `/recipes/${i}`
     })
 }
+
+// Marcar a Div que vai receber a classe CSS
+const content = document.querySelectorAll('.content')
+
+// Marcar os buttons
+const buttons = document.querySelectorAll('button')
+
+//Laço de repetição para cada click
+for (let [i,button] of buttons.entries()) {
+    // O método entries() retorna um novo objeto Array Iterator que contém os pares chave/valor para cada índice no array
+    
+    button.addEventListener("click", function() {
+        
+        if(content[i].classList.contains("show")){
+            content[i].classList.remove('show')
+            content[i].classList.add('hide')
+            button.innerHTML= "MOSTRAR"
+        } else {
+            content[i].classList.add('show')
+            content[i].classList.remove('hide')
+            button.innerHTML= "ESCONDER"
+        }
+
+    })  
+}
